4d3210d502ebb4af79a86fd24ae7520259f2b3cb
makhila8919-lang <makhila8919@gmail.com>
2026-07-15 00:36:29 +0530

    Initialize configuration settings in config.py
    Added configuration settings for file paths and secret key.

--- config.py
+++ config.py
@@ -1,1 +1,28 @@
+import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = "HDI_PROJECT_SECRET_KEY"
+
+DATASET_PATH = os.path.join(
+    BASE_DIR,
+    "dataset",
+    "Human_Development_Data.csv"
+)
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "models",
+    "hdi_model.pkl"
+)
+
+ENCODER_PATH = os.path.join(
+    BASE_DIR,
+    "models",
+    "label_encoder.pkl"
+)
+
+UPLOAD_FOLDER = os.path.join(
+    BASE_DIR,
+    "uploads"
+)
